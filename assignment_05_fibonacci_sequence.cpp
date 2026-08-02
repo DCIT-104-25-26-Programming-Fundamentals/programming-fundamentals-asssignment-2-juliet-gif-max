7f58374802f7b8c48e239fd98123c70e80f55471
juliet-gif-max <julietmakafui111@gmail.com>
2026-08-02 15:57:29 +0000

    push

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,64 @@
 #include <iostream>
 using namespace std;
 
+void printFibonacci(int n) {
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return;
+    }
+
+    int first = 0, second = 1;
+
+    cout << "Fibonacci sequence: ";
+    for (int i = 0; i < n; i++) {
+        cout << first;
+        if (i < n - 1) {
+            cout << " ";
+        }
+
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+    cout << endl;
+}
+
+bool isFibonacci(int num) {
+    if (num < 0) {
+        return false;
+    }
+
+    if (num == 0 || num == 1) {
+        return true;
+    }
+
+    int a = 0, b = 1;
+    while (b < num) {
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return (b == num);
+}
+
+int main() {
+    int n;
+    int value;
+
+    cout << "How many terms? ";
+    cin >> n;
+    printFibonacci(n);
+
+    cout << "Enter a number to check: ";
+    cin >> value;
+
+    if (isFibonacci(value)) {
+        cout << value << " is a Fibonacci number." << endl;
+    } else {
+        cout << value << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
+
